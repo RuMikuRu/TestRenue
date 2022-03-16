f64d09c96a132f9d596201453cfc268ea9a48a3d
RuMikuRu <43915493+RuMikuRu@users.noreply.github.com>
2022-03-16 22:17:32 +0300

    1.0
    Добавил поиск (алгоритм Бойера-Мура)

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try{
+            File file = new File("src/main/resources/airports.dat");
+
+            FileReader reader = new FileReader(file);
+            BufferedReader readerStr = new BufferedReader(reader);
+
+            String line = readerStr.readLine();
+            int i=1;
+            long time = System.currentTimeMillis();
+            while(line!=null) {
+                if(Parser.getFirstEntry(line, "Boi")>0)
+                {
+                    System.out.println("строка " + i);
+                }
+
+
+                line = readerStr.readLine();
+                i++;
+            }
+            System.out.println(i);
+            System.out.println(System.currentTimeMillis() - time);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //System.out.print(Parser.getFirstEntry("HeSheIt", "She"));
+    }
+}
