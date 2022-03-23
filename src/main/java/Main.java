1cbd08d29b57ab3390f82687ea9d0c248f168d91
RuMikuRu <43915493+RuMikuRu@users.noreply.github.com>
2022-03-23 03:35:03 +0300

    Я правда старался

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,19 +10,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try{
             Scanner scanner = new Scanner(System.in);
-            //String fileName = scanner.next();
+            String textSearch = scanner.next();
+            System.out.println("В ведите строку...");
             Path file = Paths.get("src/main/resources/airports.dat");
             int column = Integer.parseInt(args[0]);
+
             //String content = Files.lines(Path.of(String.valueOf(file))).sorted(Comparator.comparing(line->Integer.parseInt(line.split(",")[1])))
                  //   .collect(Collectors.joining("\n"));
             //Files.write(Paths.get("Sorted"),content.getBytes());
 
             SortingFileByColumn sortingFileByColumn = new SortingFileByColumn(column,file);
             ArrayList<Map.Entry<Integer, String>> sortArray = new ArrayList<Map.Entry<Integer, String>>(sortingFileByColumn.Sort());
-            Search searching = new Search(args[1], sortArray, file, column);
+            Search searching = new Search(textSearch, sortArray, file, column);
+
             long time = System.currentTimeMillis();
             searching.searching();
             System.out.println("Время затраченное на поиск " + (System.currentTimeMillis()-time)+" мс");
+
         } catch (IOException | ExecutionException | InterruptedException ex) {
             //ex.printStackTrace();
             System.out.println("Файл не существует");
